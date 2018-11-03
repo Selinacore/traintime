f1226313907b6c422b3ae9b180080074d9e1fbac
selinacore <ms.selinamaes@gmail.com>
2018-11-02 22:20:29 -0600

    edit added readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # traintime
+<h2>Welcome to the Clocktrain!</h2>
+<h3>Take a ride on the gateway to the mulitverse!</h3>
+<h3>This train will take you forwards and backwards and inside or out.</h3>
+<h3>But watch out for time bombs that will take you to a random destination at a random time.</h3>
+<h3>Where will you go if you could go to anytime or place?</h3>
--- assets/logic.js
+++ assets/logic.js
@@ -1,11 +1,3 @@
-// Steps to complete:
-
-// 1. Initialize Firebase
-// 2. Create button for adding new employees - then update the html + update the database
-// 3. Create a way to retrieve employees from the employee database.
-// 4. Create a way to calculate the months worked. Using difference between start and current time.
-//    Then use moment.js formatting to set difference in months.
-// 5. Calculate Total billed
 
 
   // Initialize Firebase
@@ -67,7 +59,7 @@
     var trainTime = childSnapshot.val().time;
     var trainFreq = childSnapshot.val().frequency;
   
-    // Employee Info
+    // Train Info
     console.log(trainName);
     console.log(trainDest);
     console.log(trainTime);
@@ -99,11 +91,4 @@
     $("#train-table > tbody").append(newRow);
   });
   
-  // Example Time Math
-  // -----------------------------------------------------------------------------
-  // Assume Employee start date of January 1, 2015
-  // Assume current date is March 1, 2016
-  
-  // We know that this is 15 months.
-  // Now we will create code in moment.js to confirm that any attempt we use meets this test case
   
